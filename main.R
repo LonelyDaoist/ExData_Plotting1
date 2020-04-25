af0f895eee6d16acf7a5bc8c0752697a0822557f
LonelyDaoist <ghabaroughassen@gmail.com>
2020-04-25 21:10:59 +0000

    typo

--- main.R
+++ main.R
@@ -6,7 +6,6 @@ source("plot4.R")
 # extract only the entries from 1/2/2007 til 2/2/2007
 # For Unix-based OS
 system('cat household_power_consumption.txt | grep "^[12]/2/2007" > data.txt ')
-# For Windows
 
 # Loading the data
 col.names <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
